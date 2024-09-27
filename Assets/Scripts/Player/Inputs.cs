1c8ca05f168df8279424361bac291ea9d9112022
falseDev <64366844+falseAge@users.noreply.github.com>
2024-09-27 14:06:56 +0500

    create player movement

---
+++ Assets/Scripts/Player/Inputs.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public class Inputs : MonoBehaviour
+{
+    public UnityEvent attackEvent = new();
+    public UnityEvent jumpEvent = new();
+    public Vector2 move;
+    public Vector2 look;
+    public bool attack;
+    public bool jump;
+
+    public void OnMove(InputValue value)
+    {
+        move = value.Get<Vector2>();
+    }
+
+    public void OnLook(InputValue value)
+    {
+        look = value.Get<Vector2>();
+    }
+    
+    public void OnAttack(InputValue value)
+    {
+        attack = value.isPressed;
+        attackEvent?.Invoke();
+    }
+
+    public void OnJump(InputValue value)
+    {
+        jump = value.isPressed;
+        jumpEvent?.Invoke();
+    }
+}
